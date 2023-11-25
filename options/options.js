0bad293c8046f72be10c0124575b86b6d474dfc2
Crowdbotics <team@crowdbotics.com>
2023-11-25 00:25:31 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "retainerbyte-44746",
+  url: "https://retainerbyte-44746.botics.co",
+  api: "https://retainerbyte-44746.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
